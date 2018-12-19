ba0dafbcfbb7102509c21b35683b954280d6da21
Mark Langhamer <langhamerm@gmail.com>
2018-12-19 16:09:27 -0600

    WEDNESDAY

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,9 @@
 var startScreen;
 var gameHTML;
 var counter = 30;
-var loveFactArray = ["I love that you said yes to Mello Part II for your birthday/our first date. Thanks for taking a chance on me!", "I love your stoney baloney ass",];
-var musicArray = [new Audio("assets/music/Marshmello_-_Ritual_feat_Wrabel_Lyric_Video[Mp3Converter.net].mp3"), new Audio("assets/music/Potluck f_Kottonmouth Kings  - Stoner Bitch.mp3"),];
-var imageArray = ["<img src='assets/images/firstdate.png'", "<img src='assets/images/stoneybaloney.jpg'",];
+var loveFactArray = ["I love that you said yes to Mello Part II for your birthday/our first date. Thanks for taking a chance on me!", "I love your stoney baloney ass. Remember those pot lotion massages in Denver?", "I love that my dog loves you. But concerned he might love you more lol......", "I love your taste in music! Music is how we met and is something we continue to bond over and enjoy together everyday.", "I love that I can meet you at the trap, the mall, where ever! It's always going down. LOL I love that I can go anywhere with you.", "Most importantly, you're my best friend, my rock, my everything. You've stuck by me through the best and worst of times and I never want to leave your side and I want to bring as much happiness as possible for the rest of our lives."];
+var musicArray = [new Audio("assets/music/Marshmello_-_Ritual_feat_Wrabel_Lyric_Video[Mp3Converter.net].mp3"), new Audio("assets/music/Potluck f_Kottonmouth Kings  - Stoner Bitch.mp3"), new Audio("assets/music/01 Who Let The Dogs Out.mp3"), new Audio("assets/music/07 Gramatik - Obviously.mp3"), new Audio("assets/music/Yung Joc - It's Goin Down (GoodSex Remix) [EXTENDED MIX].wav"), new Audio("assets/music/Dada Life - Feed The Dada (Radio Edit).mp3")];
+var imageArray = ["<img src='assets/images/firstdate.png'>", "<img src='assets/images/stoneybaloney.jpg'>", "<img src='assets/images/doggo.png'>", "<img src='assets/images/scamp2017.jpg'>", "<img src='assets/images/pumpkin.jpg'>", "<img src='assets/images/dadabae.jpg'>"];
 var questionCounter = 0;
 var Timer;
 $(document).ready(function () {
@@ -41,7 +41,7 @@ function countdown() {
     }
 };
 function wait() {
-    if (questionCounter < 7) {
+    if (questionCounter < 5) {
         questionCounter++;
         gameStart();
         counter = 30;
@@ -51,10 +51,15 @@ function wait() {
         finalScreen();
     }
 };
+var subtronics = new Audio("assets/music/Subtronics - Damage Report.mp3");
+var mellosuit = "<img id='meme' src='assets/images/mellosuit.jpg'";
+var mozz = "<img id='meme' src='assets/images/riddim.jpg'";
 function finalScreen() {
-    gameHTML = "<p id='texties' class='text-center'>I LOVE YOU SO MUCH BABES HAPPY BIRTHDAY AND HERE'S TO MANY MORE!</p>";
+    gameHTML = "<p id='texties' class='text-center'>I LOVE YOU BABES! HAPPY 2 YEAR ANNIVERSARY AND HERE'S TO MANY MORE!</p>" +
+        "<p id='sexties' class='text-center'>Mama always told me. Get a woman who's mello in the streets and RIDDIM in the sheets.</p>" + "<div id='mello'>" + mellosuit + "/div>" + "<div id='mozz'>" + mozz + "</div>";
     $(".gameContent").html(gameHTML);
+    subtronics.play();
 };
 function timeUpLoss() {
-	setTimeout(wait, 2000);
+    setTimeout(wait, 2000);
 };
